76c4c62d17ab8129aedea506701eee5870f55bac
Your Name <you@example.com>
2025-05-14 10:01:43 +0000

    set login, register, game and index result pages

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
-router.get('/trivia', async (req, res) => {
-  try {
-    const response = await fetch('https://opentdb.com/api.php?amount=50');
-    const data = await response.json();
-    const questionData = data.results[0];
-
-    res.json({
-      question: questionData.question,
-      correct_answer: questionData.correct_answer,
-      options: shuffle([
-        ...questionData.incorrect_answers,
-        questionData.correct_answer
-      ])
-    });
-  } catch (error) {
-    res.status(500).send('Failed to fetch trivia');
-  }
-});
-
 module.exports = router;
