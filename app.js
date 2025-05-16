7c371ff28c7f0dcb3236fa7f4ca4fdcf28dd54ad
Your Name <you@example.com>
2025-05-16 04:46:57 +0000

    API

--- app.js
+++ app.js
@@ -17,6 +17,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/trivia', require('./routes/trivia'));
-
 module.exports = app;
